bb9034751c9a92bbca6e266dfff357032c6be14d
Chris Marcok <chris.marcok@gmail.com>
2019-01-13 23:20:23 -0500

    google OAuth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://chris:qwerty123@ds155864.mlab.com:55864/storybooks-prod',
+  googleClientID:'126935348367-6ds5033ktf8vvkqmrq2fs95earfg4jcb.apps.googleusercontent.com',
+  googleClientSecret:'xL3QUVYmcUVVf44Yw095QIen'
+}
