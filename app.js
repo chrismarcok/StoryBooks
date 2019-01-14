65923f2d053d1a857b29e857a461bbabe240ad60
Chris Marcok <chris.marcok@gmail.com>
2019-01-14 00:09:41 -0500

    logout

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
+// Load User Model
+require('./models/User')
 
 // Passport config
 require('./config/passport')(passport);
@@ -9,6 +13,16 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+mongoose.promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch( err => console.log(err));
 
 const app = express();
 
@@ -16,11 +30,27 @@ app.get('/', (req, res) => {
   res.send("it works");
 });
 
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+})
+
 // Use Routes
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
-
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
--- routes/auth.js
+++ routes/auth.js
@@ -13,4 +13,17 @@ router.get('/google/callback',
     res.redirect('/dashboard');
   });
 
+router.get('/verify', (req, res) => {
+  if (req.user){
+    console.log(req.user);
+  } else {
+    console.log("can't")
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
